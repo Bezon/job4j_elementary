a55f616d6ae8bbb848f259e746e33e074521580d
Bezon <bezonbezon@yandex.ru>
2022-12-04 20:36:31 +0300

    7. Расстояние между точками в системе координат [#188 #367244] ошибка checkstyle

--- src/main/java/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class TrgArea {
     public static double area(double a, double b, double c) {
-        double p = (a + b + c)/2;
+        double p = (a + b + c) / 2;
         double four = Math.sqrt(p * (p - a) * (p - b) * (p - c));
         double rsl = four;
         return rsl;
