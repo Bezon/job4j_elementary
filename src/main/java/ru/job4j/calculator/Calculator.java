9a65b54b2192b32f3883a8faae87240f720b1dab
Bezon <bezonbezon@yandex.ru>
2022-12-04 10:52:41 +0300

    8. Стороны прямоугольника. [#137735]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -2,6 +2,15 @@ package ru.job4j.calculator;
 
 public class Calculator {
     public static void main(String[] args) {
+        byte age = 30;
+        char gender = 'М';
+        float height = 2.04F;
+        short numberOfBricks = 21000;
+        double numberOfBacteries = 3.452E+21;
+        double disToSpaceObj = 8.672E+25;
+        boolean workPermit = true;
+        long fileSize = 324_234_234;
+
         int six = 6;
         int four = 4;
         int five = 5;
