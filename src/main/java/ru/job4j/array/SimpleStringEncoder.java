da24fc147a17dbaa40434c26fa09dde323d28a43
Bezon <bezonbezon@yandex.ru>
2024-06-27 14:17:10 +0300

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049]

--- src/main/java/ru/job4j/array/SimpleStringEncoder.java
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -3,14 +3,13 @@ package ru.job4j.array;
 public class SimpleStringEncoder {
 
     public static String encode(String input) {
-        String result = "";
+        String result = input.substring(0, 1);
         char symbol = input.charAt(0);
         int counter = 1;
-        result = result + symbol;
         for (int i = 1; i < input.length(); i++) {
             if (symbol == input.charAt(i)) {
                 counter++;
-            } else if (symbol != input.charAt(i)) {
+            } else {
                 if (counter > 1) {
                     result = result + counter;
                 }
